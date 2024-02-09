63ac3edc628baf006c6f54b74ed5b6140ad8ebfd
eivindwoien <maybonofficial@gmail.com>
2024-02-09 12:51:50 +0100

    Updated Version

--- src/components/links.jsx
+++ src/components/links.jsx
@@ -12,7 +12,7 @@ const linksData = [
 export default function Links() {
 	return (
 		<div className=" fixed right-0 top-1/2 -translate-y-1/2 z-50 flex flex-col gap-3 sm:gap-4 p-3 sm:p-4 bg-neutral-400 bg-opacity-10 rounded-l-2xl backdrop-blur-lg">
-			{linksData.map((link, index) => (
+			{linksData.ma7p((link, index) => (
 				<a key={index} href={link.url} target="_blank" className="hover:scale-105 brightness-[0.9] hover:brightness-100">
 					<img className=" h-4 sm:h-8 " style={{filter: link.style}} src={link.img} alt={`Logo ${index}`} />
 				</a>

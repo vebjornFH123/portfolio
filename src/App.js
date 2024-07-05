3243892914a44e2573b8c7c694ec7f8d20c820a0
vebjornFH123 <vebjorn.fuhrehaugholt@gmail.com>
2024-07-05 09:27:24 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,42 @@
 import React from "react";
 
 import "./App.css";
-import Menubar from "./components/navbar.js";
-import About from "./components/about.js";
+import Menubar from "./components/navbar.jsx";
+import About from "./components/about.jsx";
 import Skills from "./components/skills";
-import Knowledge from "./components/knowledge.js";
-import Work from "./components/work.js";
+import Knowledge from "./components/knowledge.jsx";
+import Work from "./components/work.jsx";
 import Experience from "./components/experience";
 import Education from "./components/education";
-import Contact from "./components/contact";
-
-import Links from "./components/links.jsx";
-
-import ThreeScene from "./threeScene";
+import Contact from "./components/contact.jsx";
+import Home from "./components/home.jsx";
 
 export default function App() {
-	return (
-		<div className=" w-screen overflow-x-hidden">
-			<Menubar />
-			<Links />
-			{/* The pages below */}
-			<div id="home">
-				<ThreeScene />
-			</div>
-			<div id="about">
-				<About />
-			</div>
-			<div id="skills">
-				<Skills />
-			</div>
-			<Knowledge />
-			<div id="work">
-				<Work />
-			</div>
-			<div id="experience">
-				<Experience />
-			</div>
-			<div id="education">
-				<Education />
-			</div>
-			<div id="contact">
-				<Contact />
-			</div>
-		</div>
-	);
+  return (
+    <div className="w-screen overflow-hidden">
+      <Menubar />
+      {/* The pages below */}
+      <div id="home">
+        <Home />
+      </div>
+      <div id="about">
+        <About />
+      </div>
+      <div id="skills">
+        <Skills />
+      </div>
+      <div id="work">
+        <Work />
+      </div>
+      <div id="experience">
+        <Experience />
+      </div>
+      <div id="education">
+        <Education />
+      </div>
+      <div id="contact">
+        <Contact />
+      </div>
+    </div>
+  );
 }
